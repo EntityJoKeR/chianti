62b3b0a6284fbd8ef0e4c8a801493f636284946f
EntityJoKeR <133527371+EntityJoKeR@users.noreply.github.com>
2024-08-24 10:50:50 +0500

    Update BottomBlock.jsx

--- src/components/blocks/bottomBlock/BottomBlock.jsx
+++ src/components/blocks/bottomBlock/BottomBlock.jsx
@@ -15,14 +15,27 @@ const BottomBlock = () => {
 
             <div className={styles.card}>
                 <span className={styles.num}>02</span>
+                <h3 className={styles.text}>   
+                    Ужин -<br/>
+                    <span className={styles.word}>
+                    Концерт
+                    </span>
             </div>
 
             <div className={styles.card}>
                 <span className={styles.num}>03</span>
+                <h3 className={styles.text}>   
+                    Дижестив -<br/>
+                    <span className={styles.word}>
+                    Караоке. After party
+                    </span>
             </div>
 
             <div className={styles.card}>
                 <span className={styles.num}>04</span>
+                <h3 className={styles.text}>   
+                    Соло
+                </h3>
             </div>
         </div>
     );
