74822a0f7ab57a54694d6ebb31a932ce6dea4b94
EntityJoKeR <dimonmarin14@gmail.com>
2024-08-24 10:40:17 +0500

    ...

--- src/components/blocks/bottomBlock/BottomBlock.jsx
+++ src/components/blocks/bottomBlock/BottomBlock.jsx
@@ -2,8 +2,28 @@ import styles from "./BottomBlock.module.css"
 
 const BottomBlock = () => {
     return (
-        <div>
-            
+        <div className={styles.cards}>
+            <div className={styles.card}>
+                <span className={styles.num}>01</span>
+                <h3 className={styles.text}>   
+                    Аперитив -<br/>
+                    <span className={styles.word}>
+                    Welcome
+                    </span>
+                </h3>
+            </div>
+
+            <div className={styles.card}>
+                <span className={styles.num}>02</span>
+            </div>
+
+            <div className={styles.card}>
+                <span className={styles.num}>03</span>
+            </div>
+
+            <div className={styles.card}>
+                <span className={styles.num}>04</span>
+            </div>
         </div>
     );
 };
