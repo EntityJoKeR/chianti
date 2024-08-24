9193fd33038e008a9bb5507a9e7f831780c1c315
EntityJoKeR <133527371+EntityJoKeR@users.noreply.github.com>
2024-08-24 15:36:59 +0500

    Update TopBlock.jsx

--- src/components/blocks/topBlock/TopBlock.jsx
+++ src/components/blocks/topBlock/TopBlock.jsx
@@ -1,5 +1,5 @@
 import MainLogo from "@/components/ui/logos/mainLogo/MainLogo";
-import styles from "./TopBlock.module.css"
+import "./TopBlock.module.css"
 import ActionButton from "@/components/ui/buttons/ActionButton/ActionButton";
 import TelegramIcon from "@/components/ui/icons/TelegramIcon/TelegramIcon";
 import InstagramIcon from "@/components/ui/icons/InstagramIcon/InstagramIcon";
@@ -7,16 +7,16 @@ import WhatsappIcon from "@/components/ui/icons/WhatsapIcon/WhatsappIcon";
 
 const TopBlock = () => {
     return (
-        <div className={styles.container}>
-            <img src="/mainImg.svg" className={styles.img}/>
-            <div className={styles.content}>
+        <div className="TopBlock">
+            <img src="/mainImg.svg" className="TopBlockImg"/>
+            <div className="TopBlockContent">
                 <MainLogo></MainLogo>
-                <p className={styles.text}>
+                <p className="ContentText">
                 Кьянти может быть на аперитив, может быть под горячее, а может быть ярким дижестивом. <br/><br/>
                 Мы элегантно начинаем вечер, будоражуще его сопровождаем, и ярко завершаем. Кьянти - настроение праздника!
                 </p>
                 <ActionButton/>
-                <div className={styles.iconsCont}>
+                <div className="ContentIcons">
                     <TelegramIcon/>
                     <InstagramIcon/>
                     <WhatsappIcon/>
